3f5b0c1a9084ee7b8ddcbc084b6f7acb52f5178f
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-09 05:25:06 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Fly Pipeline
+
+[![deno module](https://shield.deno.dev/x/fly_pipeline)](https://deno.land/x/fly_pipeline)
+![deno compatibility](https://shield.deno.dev/deno/^1.34)
+[![](https://img.shields.io/codecov/c/gh/fluent-ci-templates/fly-pipeline)](https://codecov.io/gh/fluent-ci-templates/fly-pipeline)
+
+A ready-to-use CI/CD Pipeline for deploying your applications to [Fly](https://fly.io).
+
+## 🚀 Usage
+
+Run the following command:
+
+```bash
+dagger run fluentci fly_pipeline
+```
+
+## Environment Variables
+
+| Variable      | Description         |
+|---------------|---------------------|
+| FLY_API_TOKEN | Your Fly API token. |
+
+## Jobs
+
+| Job     | Description                      |
+|---------|----------------------------------|
+| deploy  | Deploys your application to Fly. |
+
+## Programmatic usage
+
+You can also use this pipeline programmatically:
+
+```typescript
+import { Client, connect } from "@dagger.io/dagger";
+import { Dagger } from "https://deno.land/x/fly_pipeline/mod.ts";
+
+const { deploy } = Dagger;
+
+function pipeline(src = ".") {
+  connect(async (client: Client) => {
+    await deploy(client, src);
+  });
+}
+
+pipeline();
+
+```
---
+++ src/dagger/jobs.ts
@@ -0,0 +1,57 @@
+import { Client } from "@dagger.io/dagger";
+import { withDevbox } from "https://deno.land/x/nix_installer_pipeline@v0.3.6/src/dagger/steps.ts";
+
+export enum Job {
+  deploy = "deploy",
+}
+
+export const deploy = async (client: Client, src = ".") => {
+  const context = client.host().directory(src);
+  const ctr = withDevbox(
+    client
+      .pipeline(Job.deploy)
+      .container()
+      .from("alpine:latest")
+      .withExec(["apk", "update"])
+      .withExec(["apk", "add", "curl", "bash"])
+      .withMountedCache("/nix", client.cacheVolume("nix"))
+      .withMountedCache("/etc/nix", client.cacheVolume("nix-etc"))
+  )
+    .withMountedCache(
+      "/root/.local/share/devbox/global",
+      client.cacheVolume("devbox-global")
+    )
+    .withExec(["devbox", "global", "add", "flyctl"])
+    .withEnvVariable("NIX_INSTALLER_NO_CHANNEL_ADD", "1")
+    .withDirectory("/app", context, {
+      exclude: [".git", ".devbox", "node_modules", ".fluentci"],
+    })
+    .withWorkdir("/app")
+    .withEnvVariable("FLY_API_TOKEN", Deno.env.get("FLY_API_TOKEN") || "")
+    .withExec(["sh", "-c", "devbox global run -- fly deploy --remote-only"]);
+
+  const result = await ctr.stdout();
+
+  console.log(result);
+};
+
+export type JobExec = (
+  client: Client,
+  src?: string
+) =>
+  | Promise<void>
+  | ((
+      client: Client,
+      src?: string,
+      options?: {
+        ignore: string[];
+      }
+    ) => Promise<void>);
+
+export const runnableJobs: Record<Job, JobExec> = {
+  [Job.deploy]: deploy,
+};
+
+export const jobDescriptions: Record<Job, string> = {
+  [Job.deploy]: "Deploy to Fly.io",
+};
